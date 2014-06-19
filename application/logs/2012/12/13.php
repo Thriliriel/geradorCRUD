f2c14a9dbf890860b4a14dea2f07849487ed8f1e
Thriliriel <wsk.shadow@gmail.com>
2014-06-18 22:11:06 -0300

    Removing Logs

--- application/logs/2012/12/13.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2012-12-13 10:27:52 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 1142 ]
-2012-12-13 10:27:52 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 1142 ]
---
-#0 /var/www/dme/index.php(109): Kohana_Request->execute()
-#1 {main}
